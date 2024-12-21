f3bfe6fe204397f94b8ac6fa7ee9cadb91309030
ffakira <55924290+ffakira@users.noreply.github.com>
2024-12-20 23:35:14 -0300

    impl: prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     plugins: {
       import: importPlugin.configs.recommended,
@@ -19,17 +19,22 @@ export default [
   },
   {
     rules: {
-      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "eol-last": ["error", "always"],
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "indent": ["error", 2],
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      indent: ["error", 2],
       "comma-dangle": ["error", "always-multiline"],
-      "eqeqeq": ["error", "always"],
+      eqeqeq: ["error", "always"],
       "arrow-spacing": ["error", { before: true, after: true }],
-      "curly": ["error", "all"],
+      curly: ["error", "all"],
       "no-trailing-spaces": ["error"],
       "array-bracket-spacing": ["error", "never"],
+      "prettier/prettier": "error",
     },
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierPlugin,
+  eslintConfigPrettier,
 ];
