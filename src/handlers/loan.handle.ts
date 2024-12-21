01247a55c8555194527366014955a50e7214b45a
ffakira <55924290+ffakira@users.noreply.github.com>
2024-12-21 00:35:14 -0300

    chore: split to handles files

---
+++ src/handlers/loan.handle.ts
@@ -0,0 +1,80 @@
+import {
+  createNewLoan,
+  getLoansByUserId,
+  loanSchema,
+} from "@/services/loan.service";
+import type { Request, Response } from "express";
+import { z } from "zod";
+
+type NewLoanUserHandleRequest = Request<
+  { userId: string },
+  unknown,
+  Omit<z.infer<typeof loanSchema>, "userId">
+>;
+
+export async function createNewLoanHandle(
+  req: NewLoanUserHandleRequest,
+  res: Response,
+) {
+  const { userId } = req.params;
+  let { startDate, endDate } = req.body;
+  startDate = new Date(startDate);
+  endDate = new Date(endDate);
+
+  const input = loanSchema.safeParse({
+    ...req.body,
+    userId,
+    startDate,
+    endDate,
+  });
+
+  if (!input.success) {
+    const errorMessages = input.error.errors.map((error) => error.message);
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: errorMessages,
+    });
+  }
+
+  try {
+    const response = await createNewLoan(input.data);
+    return res.status(response.code).json(response);
+  } catch (err) {
+    console.error("[error@/api/loan/new]: error creating a loan", err);
+    return res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Error creating loan",
+    });
+  }
+}
+
+type GetLoanByUserIdRequest = Request<{ userId: string }, unknown, unknown>;
+
+export async function getLoanByUserIdHandle(
+  req: GetLoanByUserIdRequest,
+  res: Response,
+) {
+  const { userId } = req.params;
+
+  if (!userId) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Invalid input",
+    });
+  }
+
+  try {
+    const response = await getLoansByUserId(userId);
+    return res.status(response.code).json(response);
+  } catch (err) {
+    console.error("[error@/api/loan]: error fetching loans", err);
+    return res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Error fetching loans",
+    });
+  }
+}
--- src/routes/loan.routes.ts
+++ src/routes/loan.routes.ts
@@ -1,69 +1,12 @@
-import { Router, type Request, type Response } from "express";
+import { Router } from "express";
 import {
-  createNewLoan,
-  getLoansByUserId,
-  loanSchema,
-} from "@/services/loan.service";
+  createNewLoanHandle,
+  getLoanByUserIdHandle,
+} from "@/handlers/loan.handle";
 
 const router = Router();
 
-router.post("/new/user/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-  let { startDate, endDate } = req.body;
-  startDate = new Date(startDate);
-  endDate = new Date(endDate);
-
-  const input = loanSchema.safeParse({
-    ...req.body,
-    userId,
-    startDate,
-    endDate,
-  });
-
-  if (!input.success) {
-    const errorMessages = input.error.errors.map((error) => error.message);
-    return res.status(400).json({
-      status: "error",
-      code: 400,
-      message: errorMessages,
-    });
-  }
-
-  try {
-    const response = await createNewLoan(input.data);
-    return res.status(response.code).json(response);
-  } catch (err) {
-    console.error("[error@/api/loan/new]: error creating a loan", err);
-    return res.status(500).json({
-      status: "error",
-      code: 500,
-      message: "Error creating loan",
-    });
-  }
-});
-
-router.get("/user/:userId", async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  if (!userId) {
-    return res.status(400).json({
-      status: "error",
-      code: 400,
-      message: "Invalid input",
-    });
-  }
-
-  try {
-    const response = await getLoansByUserId(userId);
-    return res.status(response.code).json(response);
-  } catch (err) {
-    console.error("[error@/api/loan]: error fetching loans", err);
-    return res.status(500).json({
-      status: "error",
-      code: 500,
-      message: "Error fetching loans",
-    });
-  }
-});
+router.post("/new/user/:userId", createNewLoanHandle);
+router.get("/user/:userId", getLoanByUserIdHandle);
 
 export default router;
